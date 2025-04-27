39025d7680c5a94bb430551a01ea1e1525cd0624
emilygmosley211 <mosley.e078056@stu.sanjac.edu>
2025-04-27 18:36:38 +0000

    Created menu-driven interface

--- superhero_database.cpp
+++ superhero_database.cpp
@@ -32,3 +32,31 @@ string info[SIZE][2] = {
     {"1961", "DC"}, {"1966", "DC"}, {"1989", "Marvel"}, {"2001", "Marvel"},
     {"2001", "Marvel"}, {"1997", "DC"}
 };
+// Function Prototypes
+void displayAll(bool toUpper = false);
+void searchByName();
+void searchByPower();
+void sortAlphabetically();
+void displayMenu();
+string toLowerCase(string str);
+string toUpperCase(string str);
+int main() {
+    int choice;
+    do {
+        displayMenu();
+        cin >> choice;
+        cin.ignore(); // Clean newline from buffer
+
+        switch (choice) {
+            case 1: searchByName(); break;
+            case 2: searchByPower(); break;
+            case 3: sortAlphabetically(); break;
+            case 4: displayAll(true); break;
+            case 5: displayAll(false); break;
+            case 6: cout << "Exiting program. Goodbye!" << endl; break;
+            default: cout << "Invalid choice. Try again.\n"; break;
+        }
+        cout << "\n";
+    } while (choice != 6);
+    return 0;
+}
